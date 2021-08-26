7be3c09e57fd2d0e4ad4b6fa90b7901edf5e0aed
Bambi007 <dziedziochs@gmail.com>
2021-08-26 14:07:51 +0200

    comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// FAQ i Features dostępne dopiero po pierwszym etapie kampanii reklamowej dla Tease
+
 import './assets/css/style.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Header } from './components/header';
 import { Hero } from './components/hero';
 import { About } from './components/about';
+// uzupełnić Features o ekran z telefonu o wymiarach dokładnie takich jak podstawiona grafika.
 // import { Features } from './components/features';
 // import { FAQ } from './components/faq';
 import { Newsletter } from './components/newsletter';
